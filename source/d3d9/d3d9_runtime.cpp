c079a784d33c3d6160784cc04ac58853c184d860
Thalixte <35728054+Thalixte@users.noreply.github.com>
2018-04-05 13:42:51 +0200

    Update d3d9_runtime.cpp

--- source/d3d9/d3d9_runtime.cpp
+++ source/d3d9/d3d9_runtime.cpp
@@ -1,7 +1,7 @@
 /**
-* Copyright (C) 2014 Patrick Mours. All rights reserved.
-* License: https://github.com/crosire/reshade#license
-*/
+ * Copyright (C) 2014 Patrick Mours. All rights reserved.
+ * License: https://github.com/crosire/reshade#license
+ */
 
 #include "log.hpp"
 #include "d3d9_runtime.hpp"
@@ -55,12 +55,12 @@ namespace reshade::d3d9
 		{
 			switch (_backbuffer_format)
 			{
-			case D3DFMT_X8R8G8B8:
-				_backbuffer_format = D3DFMT_A8R8G8B8;
-				break;
-			case D3DFMT_X8B8G8R8:
-				_backbuffer_format = D3DFMT_A8B8G8R8;
-				break;
+			        case D3DFMT_X8R8G8B8:
+				        _backbuffer_format = D3DFMT_A8R8G8B8;
+				        break;
+			        case D3DFMT_X8B8G8R8:
+				        _backbuffer_format = D3DFMT_A8B8G8R8;
+				        break;
 			}
 
 			hr = _device->CreateRenderTarget(_width, _height, _backbuffer_format, D3DMULTISAMPLE_NONE, 0, FALSE, &_backbuffer_resolved, nullptr);
@@ -416,19 +416,19 @@ namespace reshade::d3d9
 	{
 		switch (type)
 		{
-		case D3DPT_LINELIST:
-			vertices *= 2;
-			break;
-		case D3DPT_LINESTRIP:
-			vertices += 1;
-			break;
-		case D3DPT_TRIANGLELIST:
-			vertices *= 3;
-			break;
-		case D3DPT_TRIANGLESTRIP:
-		case D3DPT_TRIANGLEFAN:
-			vertices += 2;
-			break;
+		        case D3DPT_LINELIST:
+			        vertices *= 2;
+			        break;
+		        case D3DPT_LINESTRIP:
+			        vertices += 1;
+			        break;
+		        case D3DPT_TRIANGLELIST:
+			        vertices *= 3;
+			        break;
+		        case D3DPT_TRIANGLESTRIP:
+		        case D3DPT_TRIANGLEFAN:
+			        vertices += 2;
+          			break;
 		}
 
 		_vertices += vertices;
@@ -479,7 +479,7 @@ namespace reshade::d3d9
 					return;
 				}
 			}
-
+       
 			depthstencil->AddRef();
 
 			// Begin tracking
